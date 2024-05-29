1e4601cc874b1577eb3a11e4a5d155347ab67cc0
UnoRamos <150862634+UnoRamos@users.noreply.github.com>
2024-05-30 03:41:02 +0800

    Update render.sh

--- render.sh
+++ render.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # exit on errorset -o errexit
 
-npm install express body-parser ejs puppeteer && npx puppeteer install chrome
+npm install 
 # npm run build # uncomment if required
 
 # Store/pull Puppeteer cache with build cache
