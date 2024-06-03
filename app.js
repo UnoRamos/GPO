ebc472f0f343be11d5fadbae5020b0cf73ce3643
UnoRamos <150862634+UnoRamos@users.noreply.github.com>
2024-06-03 23:31:33 +0800

    Update app.js

--- app.js
+++ app.js
@@ -13,40 +13,40 @@ app.use(bodyParser.urlencoded({ extended: true }));
 let browserInstance; // Reuse browser instance
 
 app.get('/', (req, res) => {
-  res.render('form');
+    res.render('form');
 });
 
 app.post('/generate-pdf', async (req, res) => {
-  const data = req.body;
+    const data = req.body;
 
-  try {
-    const html = await ejs.renderFile(path.join(projectRoot, 'views', 'pdfTemplate.ejs'), data);
+    try {
+        const html = await ejs.renderFile(path.join(__dirname, 'views', 'pdfTemplate.ejs'), data);
 
-    if (!browserInstance) {
-      browserInstance = await puppeteer.launch({
-        headless: true, // Run in headless mode
-        args: ['--no-sandbox', '--disable-setuid-sandbox'] // Required for Linux environments
-      });
+        if (!browserInstance) {
+            browserInstance = await puppeteer.launch({
+                headless: true, // Run in headless mode
+                args: ['--no-sandbox', '--disable-setuid-sandbox'] // Required for Linux environments
+            });
+        }
+
+        const page = await browserInstance.newPage();
+        await page.setContent(html);
+        
+        // Set a longer timeout duration
+        const pdf = await page.pdf({ format: 'A4', timeout: 9000000 });
+
+        res.set({
+            'Content-Type': 'application/pdf',
+            'Content-Disposition': `attachment; filename=application.pdf`,
+        });
+        res.send(pdf);
+    } catch (error) {
+        console.error('PDF generation error:', error);
+        res.status(500).send('PDF generation error');
     }
-
-    const page = await browserInstance.newPage();
-    await page.setContent(html);
-  
-    // Set a longer timeout duration
-    const pdf = await page.pdf({ format: 'A4', timeout: 9000000 });
-
-    res.set({
-      'Content-Type': 'application/pdf',
-      'Content-Disposition': `attachment; filename=application.pdf`,
-    });
-    res.send(pdf);
-  } catch (error) {
-    console.error('PDF generation error:', error);
-    res.status(500).send('PDF generation error');
-  }
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
